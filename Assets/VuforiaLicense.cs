f27b21565b69cc9432c0d0237d9900630c04dd5e
Vishnu <phashekeabetswe@gmail.com>
2025-07-16 13:26:33 +0200

    localization

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVVGeHJyRC8vLy8vQUFBQm1RTmJRVUpHUjA0aHJ2ekRvN2k3WExJbHBFMGFoemZwajI4enZ0OWVDQlJYWWg5aXh4T0hibVFSM2pkeUdycUVTQjRvNFdpK3lCRjNaWGJQYjB3NWJIM2dRTU5PMWpVRE11NWpGVWVWYzRqa2dpOEtXallSckMxVWFaOWYwWFhuWUJCUG9jcVArT2hvSURUNURzM1FLOXlUQlNSRVRXWmxnTFNpYnNnWnJveld3MnhHWEJYeGo5UHNsNS9jTWgzaEZXQXppdkhTNVlpcTNZU0o2eWcyWGhZMlpVaW8xMDFRbytvRDJVb2VQZHNwN3R4a21UM3dnLytrazdtY3VhYWc4ODdjS0M3dXFkMU8yeko5SUpjN28xWVFibjVuRnZQMXZMc3FsTTRVTTRDWjJXR0ozNUJYbHhXZFZ5Y1lUOTY5OXdxMnVxYVJvTjl5Q29nc1FoNXF2THFDenREcVV0cDdVVllzVHJPcGFiS2E=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
